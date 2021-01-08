93e0b9970e2f6f392e8677b0c876512380b4b3b0
pcotte <pcotte@advestis.com>
2021-01-08 17:00:49 +0100

    new: dev: added conftest.py example

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -3,6 +3,7 @@ import math
 import pytest
 
 
+# noinspection PyUnusedLocal
 @pytest.mark.parametrize(
     "a, b, expected_r, expected_theta",
     [
@@ -10,7 +11,7 @@ import pytest
         (4, 5, 6.403124, 0.896055)
     ]
 )
-def test_init(a, b, expected_r, expected_theta):
+def test_init(fix1, a, b, expected_r, expected_theta):
     print("\n\n\nTesting init...\n")
     znumber = Complex(a, b)
     assert znumber.a == a
@@ -19,6 +20,7 @@ def test_init(a, b, expected_r, expected_theta):
     assert round(znumber.theta, 6) == expected_theta
 
 
+# noinspection PyUnusedLocal
 @pytest.mark.parametrize(
     "r, theta, expected_a, expected_b",
     [
@@ -26,7 +28,7 @@ def test_init(a, b, expected_r, expected_theta):
         (-1, -1, None, None)
     ]
 )
-def test_init_r(r, theta, expected_a, expected_b):
+def test_init_r(fix1, r, theta, expected_a, expected_b):
     print("\n\n\nTesting init...\n")
     if r < 0:
         with pytest.raises(ValueError):
@@ -37,19 +39,22 @@ def test_init_r(r, theta, expected_a, expected_b):
         assert round(znumber.b, 6) == expected_b
 
 
-def test_i():
+# noinspection PyUnusedLocal
+def test_i(fix1):
     print("\n\n\nTesting i...\n")
     z = 3 + 2 * i
     assert z == Complex(3, 2)
 
 
-def test_expi():
+# noinspection PyUnusedLocal
+def test_expi(fix1):
     print("\n\n\nTesting expi...\n")
     z = 3 * math.exp(2 * i)
     assert z == Complex(r=3, theta=2)
 
 
-def test_logi():
+# noinspection PyUnusedLocal
+def test_logi(fix1):
     print("\n\n\nTesting logi...\n")
     z = Complex(r=3, theta=2)
     # noinspection PyTypeChecker
