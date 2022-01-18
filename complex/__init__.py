b0501d0f2687f7752dfb0c7e6f809c5e7a3da90e
pcotte <pcotte@advestis.com>
2022-01-18 15:19:20 +0100

    chg: better docstrings

--- complex/__init__.py
+++ complex/__init__.py
@@ -1,1 +1,18 @@
+"""Implementation of the notion of complex number
+
+>>> from complex import Complex
+>>> znumber = Complex(3, 4)
+>>> znumber_fromstring = Complex(s="3+4i")
+>>> znumber_fromstring_cos = Complex(s="3cos(4) + 4isin(1)")
+>>> znumber_fromstring_exp = Complex(s="5e^3.1415926i")
+>>> znumber + znumber_fromstring
+>>> z_conj = znumber.conjugate
+"""
+
+
 from .complex import Complex, i
+
+try:
+    from ._version import __version__
+except ImportError:
+    pass
